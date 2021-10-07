7c6e94044cdf294b8c9ae19a0dfce5296f53c4ae
isha <ishaarun.singh@gmail.com>
2021-10-07 17:09:25 +0100

    database schema added

---
+++ database_sql/schema_sql.sql
@@ -0,0 +1,21 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "global_happiness_data" (
+    "country" VARCHAR   NOT NULL,
+    "region" VARCHAR   NOT NULL,
+    "year" INTEGER   NOT NULL,
+    "overall_rank" FLOAT   NOT NULL,
+    "happiness_score" FLOAT   NOT NULL,
+    "gdp_per_capita" FLOAT   NOT NULL,
+    "life_expectancy" FLOAT   NOT NULL
+);
+
+CREATE TABLE "global_happiness_mean_values" (
+    "overall_rank" FLOAT   NOT NULL,
+    "happiness_score" FLOAT   NOT NULL,
+    "gdp_per_capita" FLOAT   NOT NULL,
+    "life_expectancy" FLOAT   NOT NULL
+);
+
