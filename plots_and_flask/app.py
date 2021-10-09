b491cb57ab7cde0acc6d6d316307f82d38b4b0af
Jessica Uppal <jessica@Jessicas-MacBook-Pro.local>
2021-10-09 11:11:26 +0100

    updated code

--- plots_and_flask/app.py
+++ plots_and_flask/app.py
@@ -1,10 +1,6 @@
 import psycopg2
-import sys
-from  flask import Flask,render_template
-from flask import jsonify
-from config import username,password
-
-
+from flask import Flask, jsonify, render_template
+from config import password, username
 
 app = Flask(__name__)
 
@@ -13,23 +9,26 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/data_one')
-def send_data():
-    con = psycopg2.connect(f'postgresql://{username}:{password}@localhost:5432/world_happiness_rank')  
+@app.route('/data')
+def get_data():
+    con = psycopg2.connect('postgres://{}:{}@localhost:5432/world_happiness_rank'.format(username, password))  
+    print(con)
     cur = con.cursor()
     cur.execute("""select * from  global_happiness_data""")
     data = [col for col in cur]
     cur.close()
     return jsonify(data)
 
-@app.route('/data_two')
+@app.route('/mean_data')
 def send_data():
-    con = psycopg2.connect(f'postgresql://{username}:{password}@localhost:5432/world_happiness_rank')  
+    con = psycopg2.connect('postgres://{}:{}@localhost:5432/world_happiness_rank'.format(username, password))  
+    print(con)
     cur = con.cursor()
     cur.execute("""select * from  global_happiness_mean_values""")
     data = [col for col in cur]
     cur.close()
     return jsonify(data)
+
 
 if __name__ == "__main__":
   app.run()
